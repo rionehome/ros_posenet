f005f8c65cebd55984340cd3397c83bbbb5204ef
Vytas <vytas@hansonrobotics.com>
2018-09-24 16:45:15 +0800

    less delay and ros args

--- scripts/posenet.js
+++ scripts/posenet.js
@@ -14,7 +14,7 @@ const StringMsg = rosnodejs.require('std_msgs').msg.String;
 
 
 async function run() {
-    const rosNode = await rosnodejs.initNode('/posenet')
+    const rosNode = await rosnodejs.initNode('posenet')
     // ROS function for simple recieveing node param
     const getParam = async function(key, default_value){
         if(await rosNode.hasParam(key)){
@@ -42,7 +42,7 @@ async function run() {
     let image_width = 0
     let image_height = 0
     let header = null
-    // Parameters for posenet
+    // Parameters for posenet3
     const imageScaleFactor = await getParam('~image_scale_factor', 0.5);
     const flipHorizontal = await getParam('~flip_horizontal', false);
     const outputStride = await getParam('~output_stride', 16);
@@ -50,34 +50,48 @@ async function run() {
     const scoreThreshold = await getParam('~score_threshold', 0.5);
     const nmsRadius = await getParam('~nms_radius', 20);
     // topic names
-    const camera_topic = await getParam('~topic','/camera/image_raw')
-    const output_topic = await getParam('~poses_topic','/poses')
+    const camera_topic = await getParam('~topic','camera/image_raw')
+    const output_topic = await getParam('~poses_topic','poses')
     // ROS topics
     let pub = rosNode.advertise(output_topic, StringMsg)
     //
+
+
     let sub = rosNode.subscribe(camera_topic, sensor_msgs.Image,
         (data) => {
             // TODO more encodings
             // Currently works wonly with rgb8 data
             assert(data.encoding == 'rgb8')
+            header = data.header
+            let delay = Math.floor(Date.now()) / 1000 - header.stamp.secs - header.stamp.nsecs/1000000000
+            if (delay > 0.05) return
             buffer = data.data
             newBuffer = true
-            header = data.header
             image_height = data.height
             image_width = data.width
+            //DetectingPoses()
         }
     );
     // Loop for detecting poses
+    let singleInstance = false
     const DetectingPoses = async function (){
-    if (newBuffer == false)  return
+        if (newBuffer == false)  return
+        if (singleInstance){
+            console.log("Skip")
+            return
+        }
+        singleInstance = true
+        let ts = header.stamp
         let tensor = tf.tensor3d(buffer, [image_height,image_width,3], 'int32')
         newBuffer = false
         const poses = await net.estimateMultiplePoses(tensor, imageScaleFactor, flipHorizontal, outputStride,
                                                        maxPoseDetections, scoreThreshold,nmsRadius);
         tensor.dispose();
-        pub.publish({data: stringify(poses)})
+        let msg = {ts: ts, poses:poses}
+        pub.publish({data: stringify(msg)})
+        singleInstance = false
     }
-    setInterval(DetectingPoses, 10);
+    setInterval(DetectingPoses, 50)
 }
 
 
