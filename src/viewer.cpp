b8e6b337759c140fddeb75be1f0f9e5e006eb714
MgilyA <zq6295migly@gmail.com>
2018-12-21 00:09:01 +0900

    [add]viewer追加

---
+++ src/viewer.cpp
@@ -0,0 +1,75 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Image.h"
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include "json11.hpp"
+
+using namespace std;
+
+cv::Mat color;
+
+void getImage_color(const sensor_msgs::Image::ConstPtr& msg) {
+
+	cv_bridge::CvImagePtr cv_ptr;
+
+	try {
+		// ROSからOpenCVの形式にtoCvCopy()で変換。cv_ptr->imageがcv::Matフォーマット。
+		cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+	} catch (cv_bridge::Exception& e) {
+		ROS_ERROR("cv_bridge exception: %s", e.what());
+		return;
+	}
+
+	/*if (!color.empty()) {
+		cv::imshow("Color", color );
+		cv::waitKey(1);
+	}*/
+
+
+
+	color = cv_ptr->image;
+
+}
+
+void view(const std_msgs::String::ConstPtr& msg) {
+
+	string err;
+	auto json = json11::Json::parse(msg->data, err);
+
+	if (json["poses"].array_items().size() != 0) {
+		for (auto &k : json["poses"][0]["keypoints"].array_items()) {
+			//cout << k["position"]["x"].dump() << endl;
+
+			if (std::stod(k["score"].dump()) > 0.1) {
+
+				double x = std::stod(k["position"]["x"].dump());
+				double y = std::stod(k["position"]["y"].dump());
+
+				//printf("%f %f\n", x, y );
+				cv::circle(color, cv::Point(x, y), 10, cv::Scalar(0, 0, 255), -1);
+				cv::imshow("Color", color );
+				cv::waitKey(1);
+			}
+		}
+	} else {
+		printf("error\n");
+	}
+}
+
+
+int main(int argc, char **argv) {
+
+	ros::init(argc, argv, "viewer");
+
+	ros::NodeHandle n;
+
+	ros::Subscriber sub = n.subscribe("/poses", 1000, view);
+	ros::Subscriber left_sub = n.subscribe("/image_raw", 1, getImage_color);
+
+
+	ros::spin();
+
+	return 0;
+}
