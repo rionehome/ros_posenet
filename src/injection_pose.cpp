86d5cd2ca37694ec3d63adc5dd3c36c32aa8bdcf
MgilyA <zq6295migly@gmail.com>
2019-02-03 19:25:25 +0900

    [update]message追加

--- src/injection_pose.cpp
+++ src/injection_pose.cpp
@@ -6,10 +6,14 @@
 #include <iostream>
 #include <memory>
 #include "json11.hpp"
+#include "ros_posenet/Keypoint.h"
+#include "ros_posenet/Poses.h"
+#include "ros_posenet/Pose.h"
 
 using namespace std;
 
 cv::Mat depth;
+ros::Publisher injected_poses;
 
 void getImage_depth(const sensor_msgs::Image::ConstPtr& msg) {
 
@@ -30,15 +34,21 @@ void callback(const std_msgs::String::ConstPtr& msg) {
 
 	string err;
 	auto json = json11::Json::parse(msg->data, err);
+	ros_posenet::Poses poses;
 
 	if (json["poses"].array_items().size() != 0) {
 		for (auto &p : json["poses"].array_items()) {
 			for (auto &k : p["keypoints"].array_items()) {
 				if (std::stod(k["score"].dump()) > 0.5) {
 
-					double x = std::stod(k["position"]["x"].dump());
-					double y = std::stod(k["position"]["y"].dump());
-					double score = std::stod(k["score"].dump());
+					ros_posenet::Keypoint key;
+					ros_posenet::Pose pose;
+
+					key.position.x = std::stod(k["position"]["x"].dump());
+					key.position.x = std::stod(k["position"]["y"].dump());
+					key.score = std::stod(k["score"].dump());
+
+					pose.keypoints.push_back(key);
 
 					//cv::circle(depth, cv::Point(x, y), 3, cv::Scalar(0, 0, 255), -1);
 				}
@@ -58,9 +68,9 @@ int main(int argc, char **argv) {
 
 	ros::NodeHandle n;
 
-	ros::Subscriber sub = n.subscribe("/poses", 1000, callback);
+	ros::Subscriber sub = n.subscribe("/js_poses", 1000, callback);
 	ros::Subscriber left_sub = n.subscribe("/openni2/depth", 1, getImage_depth);
-
+	injected_poses = n.advertise<ros_posenet::Poses>("/ros_posenet/poses", 1000);
 
 	ros::spin();
 
