ffff416ecfcfb1b61f2717a8379d9e03ae3b8a9d
MiglyA <zq6295migly@gmail.com>
2019-03-16 17:49:16 +0900

    [add]mainファイル追加

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "sensor_msgs/Image.h"
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <memory>
+#include "json11.hpp"
+#include "ros_posenet/Keypoint.h"
+#include "ros_posenet/Poses.h"
+#include "ros_posenet/Pose.h"
+
+int main(int argc, char const *argv[]) {
+	/* code */
+	return 0;
+}
