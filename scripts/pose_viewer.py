e82ca67b2084b751c0daa7f3f30e2dcb1cf9644a
MiglyA <zq6295migly@gmail.com>
2019-08-04 23:57:09 +0900

    [update]ビジュアライザー実装

--- scripts/pose_viewer.py
+++ scripts/pose_viewer.py
@@ -1,21 +1,44 @@
 #!/usr/bin/env python
 # coding: UTF-8
+import copy
+
+import cv2
+from cv_bridge import CvBridge, CvBridgeError
 import rospy
 from ros_posenet.msg import Poses
 from sensor_msgs.msg import Image
-import matplotlib.pyplot as plt
 
 
 class Visualize:
     def __init__(self):
-        rospy.Subscriber("/ros_posenet/result", Poses, self.poses_callback)
-        rospy.Subscriber("/posenet/input", Image, self.image_callback)
+        rospy.init_node("visualize")
+        rospy.Subscriber("/ros_posenet/result", Poses, self.poses_callback, queue_size=1)
+        rospy.Subscriber("/posenet/input", Image, self.image_callback, queue_size=1)
+        self.bridge = CvBridge()
+        self.color_image = None
+        self.poses = None
     
-    def poses_callback(self):
-        pass
+    def poses_callback(self, msg):
+        # type:(Poses)->None
+        self.poses = msg.poses
     
-    def image_callback(self):
-        pass
+    def image_callback(self, msg):
+        # type:(Image)->None
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
+            self.color_image = copy.copy(cv_image)
+        except CvBridgeError as e:
+            print 'Cv_Bridge_Error:', e
+        
+        if self.poses == None:
+            return
+        
+        for pose in self.poses:
+            for key in pose.keypoints:
+                cv2.circle(self.color_image, (int(key.position.x), int(key.position.y)), 10, (255, 255, 255),
+                           thickness=-1)
+        cv2.imshow("window", self.color_image)
+        cv2.waitKey(1)
 
 
 if __name__ == '__main__':
