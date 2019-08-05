134d056617a9966bcb7118e0692a1ff7f8f552f2
MiglyA <zq6295migly@gmail.com>
2019-08-05 17:38:16 +0900

    [update]signalキャッチ追加

--- camera_posenet.sh
+++
@@ -1,3 +0,0 @@
-cd $(cd $(dirname $0);pwd)/posenet-models
-python3 -m http.server &
-roslaunch ros_posenet camera.launch
--- scripts/server.py
+++ scripts/server.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 # coding: UTF-8
 import rospkg
+import sys
+
 from flask import Flask, send_from_directory
 import rospy
+import signal
 
 app = Flask(__name__)
-
 HOST = "127.0.0.1"
+
+
+def exit_gracefully(signum, frame):
+    print signum, frame
+    sys.exit(1)
 
 
 @app.route('/<path:path>')
@@ -17,5 +24,6 @@ def media(path):
 
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, exit_gracefully)
     rospy.init_node("model_server")
     app.run(debug=False, host=HOST, port=8000)
